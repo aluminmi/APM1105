aaf39dd6b7500e245192bf0a8085a9fa2e83f368
Naomi Cuerdo <98798591+aluminmi@users.noreply.github.com>
2023-02-03 21:26:52 +0800

    Update FA1_CUERDO, NAOMI HANNAH A. .py

--- FA1_CUERDO, NAOMI HANNAH A. .py
+++ FA1_CUERDO, NAOMI HANNAH A. .py
@@ -1,20 +1,33 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+
 # In[17]:
 
-
-# exercise 
+# exercise 2
 km = float(input("Enter value in km:"))
 
 miles = km/1.61
 print("The kilometer in miles is:")
 print(float(miles))
 
+# Exercise 2 answer 1
+#There is 4.27 minutes in every km since 10km is equivalent to 42minutes and 42seconds
+minutes_per_km = 4.27
+km_per_mile = 1.61
 
+km = int(input("Enter km: "))
+
+miles = km / km_per_mile 
+hours = minutes_per_km / 60
+pace = minutes_per_km / miles
+mph = miles / hours
+
+print('The Pace/time in minutes per mile:', pace)
+print('The Average speed in miles per hour:', mph)
+
+#Exercise 3 answer 2
 # In[27]:
-
-
 
 km=float(input("Enter kilometer :"))
 m = float(input("Enter minutes:"))
